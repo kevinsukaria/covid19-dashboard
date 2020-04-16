ebe410023fdd3c89efbd45ab022b83579f14fa7d
kevinsukaria <kevinsukaria@github.com>
2020-04-16 10:08:54 +0700

    removed_api_keys

--- feeds.py
+++ feeds.py
@@ -3,13 +3,7 @@ import tweepy
 import datetime as dt
 import pandas as pd
 
-auth = tweepy.OAuthHandler(
-    'sTdMg4nUSf4brak6uuBOZJg4M',
-    'b8WQYukt00Kgl6kdTxMnx4OVlCTcD28EtsT4d2X9SOb9JomuRK')
-auth.set_access_token('1392363164-8tTU3tZByrZKVg2Sq7DP7kDSaN9CKkYkJGXhpzw',
-                      'woxdgDzBQ568OqGHXoItQCrs29d3LvtkZ7drL89WdX23e')
-
-newsapi = NewsApiClient(api_key='22b35c71e16d41e5856cd1c78f7e132f')
+newsapi = NewsApiClient(api_key='INSERT YOUR API KEY HERE')
 
 
 
@@ -39,22 +33,22 @@ def refresh_news(query, source):
     return news_data
 
 
-def refresh_tweets():
-    api = tweepy.API(auth)
-
-    public_tweets = api.search(q=['COVID-19'],
-                               lang='en',
-                               result_type='recent',
-                               count=10,
-                               tweet_mode='extended')
-    tweet_data = []
-    for tweet in public_tweets:
-        data = {
-            'created_at': (tweet.created_at + dt.timedelta(hours=7)).strftime('%A %d %B %Y | %H:%M GMT+7'),
-            'tweet': tweet.full_text,
-            'username': '@' + tweet.user.screen_name,
-            'name': tweet.user.name,
-            'pic': tweet.user.profile_image_url_https
-        }
-        tweet_data.append(data)
-    return tweet_data
+# def refresh_tweets():
+#     api = tweepy.API(auth)
+#
+#     public_tweets = api.search(q=['COVID-19'],
+#                                lang='en',
+#                                result_type='recent',
+#                                count=10,
+#                                tweet_mode='extended')
+#     tweet_data = []
+#     for tweet in public_tweets:
+#         data = {
+#             'created_at': (tweet.created_at + dt.timedelta(hours=7)).strftime('%A %d %B %Y | %H:%M GMT+7'),
+#             'tweet': tweet.full_text,
+#             'username': '@' + tweet.user.screen_name,
+#             'name': tweet.user.name,
+#             'pic': tweet.user.profile_image_url_https
+#         }
+#         tweet_data.append(data)
+#     return tweet_data
